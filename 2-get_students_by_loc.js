c4b65882e0969f7b05b6044e7434ef97662c9428
muasyasila <curtissila4@gmail.com>
2023-07-07 20:47:11 +0000

    Trpe script

--- 2-get_students_by_loc.js
+++
@@ -1,3 +0,0 @@
-export default function getStudentsByLocation(lst, city) {
-  return lst.filter((obj) => obj.location === city);
-}
--- README.md
+++
@@ -1,3 +0,0 @@
- This is a Read me
- 0x03-ES6_data_manipulation
-
